cf89f2d927dd86f09af2ea428d74b4daf85078e8
Patrick Smith <patrick.smith@kashew.net>
2018-02-14 12:31:46 -0500

    Added move_count to Board Factory

--- spec/factories/board_factory.rb
+++ spec/factories/board_factory.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     association :primary_player, factory: :user
     association :secondary_player, factory: :user
     current_player { primary_player }
+    move_count { Faker::Number.between(0, 42) }
   end
 end
