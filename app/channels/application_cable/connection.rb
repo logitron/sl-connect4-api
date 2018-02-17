7f4c060d5bc19a71d7c5ff791cfb88a647fdbe98
Patrick Smith <patrick.smith@kashew.net>
2018-02-17 10:54:20 -0500

    ActionCable Connection Setup for Authorization Access Token

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,22 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+
+    def find_verified_user
+      command = AuthorizeApiRequest
+        .call({ 'Authorization': request.params[:access_token] })
+
+      if command.success?
+        command.result 
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
