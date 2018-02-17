7b9181eb7b8d4acc9987ad5235447e48ff2e5108
Patrick Smith <patrick.smith@kashew.net>
2018-02-17 16:58:12 -0500

    Added Receiving Play for Channel

--- app/channels/game_play_channel.rb
+++ app/channels/game_play_channel.rb
@@ -2,4 +2,13 @@ class GamePlayChannel < ApplicationCable::Channel
   def subscribed
     stream_for current_user
   end
+
+  def receive(data)
+    if data.board_id && data.column_index
+      board = Board.find(data.board_id)
+
+      PlayGameColumn.call(board, data.column_index) if
+        board.current_player == current_user
+    end
+  end
 end
