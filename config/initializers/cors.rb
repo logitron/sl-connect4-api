f2a5402a032c17e51d791555d22929ec1ee86a91
Patrick Smith <patrick.smith@kashew.net>
2018-03-01 08:47:37 -0500

    Added Rack CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'sl-connect4-ui-staging.herokuapp.com',
+            'sl-connect4-ui.herokuapp.com',
+            'localhost:8080'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
